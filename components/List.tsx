80d5543af3a00f1c0ad8cc43070e2b6414a1b7a8
BevilKunG <devilbevilgamer@gmail.com>
2021-11-27 14:35:30 +0700

    add schedule demo

---
+++ components/List.tsx
@@ -0,0 +1,64 @@
+import {Droppable, Draggable} from '../libs/dnd-dynamic'
+import styles from '../styles/list.module.sass'
+
+interface ListProps {
+  data: any
+}
+
+function List({data}: ListProps) {
+  console.log(data.columns['column-0']['projectIds'])
+  return (
+    <div className="w-56 h-96 bg-white rounded-lg m-auto p-4 overflow-y-scroll">
+      <h2 className="text-gray-600 text-lg font-semibold mb-4">Projects</h2>
+
+      <Droppable droppableId={data.columns['column-0']['id']}>
+
+        {
+          (provided) => (
+            <div 
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="grid gap-1">
+              {data.columns['column-0']['projectIds'].map((projectId: string, index: number) => (
+                <Card key={projectId} {...{project: data.projects[projectId], index}} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )
+        }
+      </Droppable>
+    </div>
+  )
+}
+
+export default List
+
+interface CardProps {
+  project: any,
+  index: number
+}
+
+export function Card({project, index}: CardProps) {
+  return (
+    <Draggable draggableId={project.id} index={index}>
+      {
+        (provided) => (
+          <div 
+            style={{
+              width: '11.875rem'
+            }}
+            className={`${styles.card} relative h-10 rounded`}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}>
+            <div className="pl-2">
+              <h4 className="text-white">{project.title}</h4>
+            </div>
+
+            <div className="absolute top-1 right-1 w-2 h-2 rounded-full bg-green-400" />
+          </div>
+        )
+      }
+    </Draggable>
+  )
+}
--- components/Table.tsx
+++ components/Table.tsx
@@ -1,36 +1,108 @@
+import {Droppable} from '../libs/dnd-dynamic'
 import styles from '../styles/table.module.sass'
+import {Card} from './List'
 
-function Table() {
+interface TableProps {
+  data: any
+}
+
+function Table({data}: TableProps) {
+  const {dates, times, columns, projects} = data
+
   return (
-    <div className="grid gap-1 grid-cols-5">
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+    <div className={styles.table}>
+      
+      <div>
+        <Header dates={dates} />
+        <div className="overflow-y-scroll h-96 flex flex-row">
+          <Time times={times} />
+          {dates.map((date: string) => (
+            <Column key={date} {...{date, times, columns, projects}} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Table
 
-function Column() {
+interface HeaderProps {
+  dates: string[]
+}
+
+function Header({dates}: HeaderProps) {
+  const dayOfWeek = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THUSDAY', 'FRIDAY', 'SATURDAY']
+
   return (
-    <div className={styles.column}>
-      <div className={`${styles.header} text-white text-center font-semibold uppercase`}>
-        <h2 className="text-3xl mb-2">9</h2>
-        <h3 className="text-xs">Monday</h3>
-      </div>
-      <Row />
-      <Row />
-      <Row />
+    <div className="h-20 flex flex-row">   
+      <div className="w-20" /> 
+
+      {dates.map((date) => new Date(date))
+        .map((date) => (
+          <div key={date.getTime()} className={`${styles.header} w-48 mx-0.5 rounded-t-lg text-center font-semibold uppercase text-white`}>
+              <h2 className="text-3xl mb-2">{date.getDate()}</h2>
+              <h3 className="text-xs">{dayOfWeek[date.getDay()]}</h3>
+          </div>
+      ))}
     </div>
   )
 }
 
-function Row() {
+interface ColumnProps {
+  date: string
+  times: string[]
+  columns: any
+  projects: any
+}
+
+function Column({date, times, columns, projects}: ColumnProps) {
   return (
-    <div className="h-12 border-2 border-t-0 border-gray-300">
+    <div className={`${styles.column} w-48 mx-0.5`}>
+      {times.map((time) => (
+        <Row key={time} {...{column: columns[`column-${date}-${time}`], projects}} />
+      ))}
+    </div>
+  )
+}
+
+interface RowProps {
+  column: any
+  projects: any
+}
+
+function Row({column, projects}: RowProps) {
+  return (
+    <Droppable droppableId={column.id}>
+      {
+        (provided) => (
+          <div 
+            className="h-10 border-2 border-t-0 border-gray-300 bg-white"
+            ref={provided.innerRef}
+            {...provided.droppableProps}>
+            {provided.placeholder}
+            {column.projectIds.map((projectId: string, index: number) => (
+              <Card key={projectId} {...{project: projects[projectId], index}} />
+            ))}
+          </div>
+        )
+      }
+    </Droppable>
+  )
+}
+
+interface TimeProps {
+  times: string[]
+}
+
+function Time({times}: TimeProps) {
+  return (
+    <div className="w-20">
+      {times.map((time) => (
+        <div key={time} className="h-10 text-center font-semibold text-base text-gray-500">
+          {time}
+        </div>
+      ))}
     </div>
   )
 }
